adba991b07272746d57561c291119563ad21e7d1
Mihql <vipercoc36@gmail.com>
2024-10-13 19:41:03 +0530

    Add temperature_analyzer_longest_streak method Returns longest streak of consecutive days where the temperature increased, returning the starting day index of that streak.

--- temperature_analyzer.rb
+++ temperature_analyzer.rb
@@ -1,7 +1,13 @@
+max_temperature = 0
+result_day = nil
+result_temp = nil
+count = 0
+
 def temperature_analyzer(temperature)
   max_temperature = 0
-  result_day = nil
-  result_temp = nil
+result_day = nil
+result_temp = nil
+count = 0
 
   temperature.each_with_index do |temp, day|
     # temperature[day+1].each do |temp_day_two_index|
@@ -26,10 +32,38 @@ def temperature_analyzer(temperature)
   result = [result_day, result_temp]
 end
 
+# Return the longest streak of consecutive days where the temperature increased
+def temperature_analyzer_longest_streak(temperature)
+
+max_streak_length = 0
+max_streak_start_day = nil
+current_streak_length = 0
+current_streak_start_day = 0
+
+  temperature.each_with_index do |temp_value, temp_day|
+    if temp_day > 0 && temp_value > temperature[temp_day - 1]
+      current_streak_length += 1
+    else
+      # Reset?
+      current_streak_start_day = temp_day
+      current_streak_length = 1
+    end 
+
+    if current_streak_length > max_streak_length
+      max_streak_length = current_streak_length
+      max_streak_start_day = current_streak_start_day
+    end
+  end
+
+  max_streak_start_day
+end
+
 
 temperature = [20, 18, 25, 22, 30, 28, 35]
+temperature_streak = [20, 22, 23, 21, 24, 25, 26, 20, 21]
 # temperature = [22, 20, 20, 19, 18, 17, 16]
 p temperature_analyzer(temperature)
+p temperature_analyzer_longest_streak(temperature_streak)
 
 
 
